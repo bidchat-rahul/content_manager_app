b49c39ee15b644e4e846d57b84a5dd36a19ea1a8
bidchat-rahul <rahul.harmalkar@bidchat.com>
2023-02-21 05:12:57 -0800

    arrow

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,34 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
+import React from "react";
+
+function CompA(){
+  return <h1>CompA </h1>
 }
 
-export default HomePage
+function CompB(){
+  return <h1>CompB</h1>
+}
+
+class CompC extends React.Component {
+  render() {
+    return <h1>COmp C</h1>
+  }
+}
+
+const CompD = _ => (
+  <h3>arrow func</h3>
+) 
+
+function HomePage() {
+  return (
+    <>
+      <div>Welcome to Next.js!</div>
+      <CompA/>
+      <CompB />
+      <CompC />
+      <CompD />
+    </>
+
+  )
+}
+
+export default HomePage;
