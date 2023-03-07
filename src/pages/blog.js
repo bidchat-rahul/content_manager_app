edafea4680ebb5e06066d917b9f28b9bb9dad24c
bidchat-rahul <rahul.harmalkar@bidchat.com>
2023-03-07 01:38:07 -0800

    lec

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,10 +6,10 @@ import {
   Footer
 } from '../../component';
 
-import { resources } from '../../api/data';
+// import { resources } from '../../api/data';
 
 
-function Blog() {
+function Blog({ resources }) {
   return (
     <Layout>
       <ResourceHighlight resources={resources.slice(0,1)} />
@@ -19,5 +19,47 @@ function Blog() {
     </Layout>
   );
 }
+
+// is called build time
+// export async function getStaticProps() {
+
+//   console.log("getStaticProps called");
+//   try {
+//     const resData = await fetch("http://0.0.0.0:3000/api/resources");
+//     const data = await resData.json();
+//     return {
+//       props : {
+//         resources: data
+//       }
+//     }
+//   } catch (error) {
+//     return {
+//       props: {
+//         resources: []
+//       }
+//     }
+//   }
+// }
+
+// is called every time visit the time
+export async function getServerSideProps() {
+  console.log("getServerSideProps called");
+  try {
+    const resData = await fetch("http://0.0.0.0:3000/api/resources");
+    const data = await resData.json();
+    return {
+      props : {
+        resources: data
+      }
+    }
+  } catch (error) {
+    return {
+      props: {
+        resources: []
+      }
+    }
+  }
+}
+
 
 export default Blog;
